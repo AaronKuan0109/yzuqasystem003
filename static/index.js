1b34db241f7b4bf7c75aaef8970f4c7dc75c3b47
Aaron <157578322+AaronKuan0109@users.noreply.github.com>
2024-11-28 10:49:25 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -10,41 +10,39 @@ function copyText(text) {
     }
 }
 
-// 讓錄音按鈕添加事件監聽器
 document.getElementById('startBtn').addEventListener('click', startRecording);
 document.getElementById('stopBtn').addEventListener('click', stopRecording);
 
 let mediaRecorder;
 let audioChunks = [];
 const userInput = document.querySelector(".user-input");
-
-// 開始錄音功能
 function startRecording() {
     navigator.mediaDevices.getUserMedia({ audio: true })
         .then(stream => {
             mediaRecorder = new MediaRecorder(stream);
             mediaRecorder.start();
             audioChunks = [];
+
             mediaRecorder.addEventListener('dataavailable', event => {
                 audioChunks.push(event.data);
             });
+
             mediaRecorder.addEventListener('stop', () => {
                 const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+                const audioUrl = URL.createObjectURL(audioBlob);
                 uploadAudio(audioBlob);
             });
         })
         .catch(error => console.error('錄音啟動失敗:', error));
 }
 
-// 停止錄音功能
 function stopRecording() {
     if (mediaRecorder) {
         mediaRecorder.stop();
-        mediaRecorder.stream.getTracks().forEach(track => track.stop());
+        mediaRecorder.stream.getTracks().forEach(track => track.stop());  // 停止所有軌道，關閉麥克風
     }
 }
 
-// 上傳音頻到伺服器(app.py)
 function uploadAudio(blob) {
     const formData = new FormData();
     formData.append('audio', blob, 'recording.wav');
@@ -74,6 +72,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // 隱藏範例問題的容器
     exampleQuestionsContainer.style.display = "none";
+    
+   
 
     // 顯示範例問題的功能
     function displayExampleQuestions(questions) {
@@ -102,6 +102,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // 清除範例問題
         exampleQuestionsContainer.style.display = "none";
+       
 
         const userMessageDiv = document.createElement("div");
         userMessageDiv.classList.add("message", "user");
